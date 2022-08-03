37bc6d99a1f2a11b34e8422965c74381ca8f982d
Anthony Beech <abeech123@virginmedia.com>
2022-08-03 20:51:47 +0100

    Create leetcode.py

---
+++ leetcode.py
@@ -0,0 +1,42 @@
+#solution for finding numbers that add to target
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
+            
+            
+#running sum
+
+class Solution {
+public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            // Result at index `i` is sum of result at `i-1` and element at `i`.
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+}
+
+#think of total sum rather than calculating left and right seperately
+
+class Solution:
+    def pivotIndex(self, nums):
+        S = sum(nums)
+        leftsum = 0
+        for i, x in enumerate(nums):
+            if leftsum == (S - leftsum - x):
+                return i
+            leftsum += x
+        return -1
+      
+      
+  #string isomorphism test
+  
+  class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        return len(set(s)) == len(set(t)) == len(set(zip(s, t)))
